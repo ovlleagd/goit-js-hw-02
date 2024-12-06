541cc423abd6c048fbfe3efcfbde5cba1291475d
Oleh <tarantsovolegg@gmail.com>
2024-12-06 16:35:04 +0200

    commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function checkForSpam(message) {
+  const newMessage = message.toLowerCase();
+  const hasMySymbol = newMessage.includes("spam", "sale");
+  if (hasMySymbol) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
